75923cf847b7c0355c90dbfe9aaeada443743be7
ntrer <929727871@qq.com>
2020-04-01 16:50:11 +0800

    submit

---
+++ src/components/MyTodo.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<div v-for="(todo,index) in todos" :key="index">
+			{{todo.name}}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	 name: 'MyTodo',
+	 props: {
+	   todos: Array
+	 }
+}
+</script>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // which is lazy-loaded when the route is visited.
+	//懒加载路由
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
